6a01f36f08c1cda550c0d29b033aa8209a7722d6
yangshengkai <skyeinfo@qq.com>
2023-10-20 21:46:41 +0800

    add

---
+++ product_replace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 将work-skill软链到项目代码同级目录位置
+
+projectPath=$(dirname "$0")
+cd "$projectPath"/.. || return
+
+for dir in *; do
+  if [ -d "$dir" ];then
+    if [ "$dir" == "work-skill" ];then
+      continue
+    fi
+
+    cd "$dir" || continue
+    git checkout master;
+    git pull;
+    echo "$dir 更新完毕！"
+    echo "===================================="
+    cd ..
+  else
+    echo "$dir 此文件不是目录"
+    echo "===================================="
+  fi
+done
